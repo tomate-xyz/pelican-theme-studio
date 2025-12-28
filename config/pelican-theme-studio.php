8536fd0c2c0b51271639aab3923fc6e27d60ab26
tomate-xyz <tomatexyz@protonmail.com>
2025-12-28 05:07:00 +0100

    fix: rename env variables

--- config/pelican-theme-studio.php
+++ config/pelican-theme-studio.php
@@ -1,12 +1,11 @@
 <?php
 
 return [
-  "primary" => env("TXYZ_THEME_STUDIO_PRIMARY", "#0053cf"),
-  "gray" => env("TXYZ_THEME_STUDIO_GRAY", "#000000"),
-  "info" => env("TXYZ_THEME_STUDIO_INFO", "#0ea5e9"),
-  "success" => env("TXYZ_THEME_STUDIO_SUCCESS", "#22c55e"),
-  "warning" => env("TXYZ_THEME_STUDIO_WARNING", "#f59e0b"),
-  "danger" => env("TXYZ_THEME_STUDIO_DANGER", "#ef4444"),
-
-  "radius" => env("TXYZ_THEME_STUDIO_RADIUS", "0.50rem"),
+  "primary" => env("THEME_PRIMARY", "#0053cf"),
+  "gray" => env("THEME_GRAY", "#000000"),
+  "info" => env("THEME_INFO", "#0ea5e9"),
+  "success" => env("THEME_SUCCESS", "#22c55e"),
+  "warning" => env("THEME_WARNING", "#f59e0b"),
+  "danger" => env("THEME_DANGER", "#ef4444"),
+  "radius" => env("THEME_RADIUS", "0.50rem"),
 ];
